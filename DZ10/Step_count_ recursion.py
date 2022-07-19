40f07cc9478d316ef4ecd29948d75078c120dbfb
DavidKoban <49913976+DavidKoban@users.noreply.github.com>
2022-07-19 20:37:02 +0300

    Update Step_count_ recursion.py

--- DZ10/Step_count_ recursion.py
+++ DZ10/Step_count_ recursion.py
@@ -1,8 +1,12 @@
 def options_count(n):
-    if n == 0 or n == 1:
-        return 1
+    if n < 0:
+        return 0
+    elif n == 1 or n == 2:
+        return n
+    elif n == 3:
+        return options_count(n - 1) + options_count(n - 2) +1
     else:
-        return options_count(n-1) + options_count(n-2)
+        return options_count(n-1) + options_count(n-2) + options_count(n-3)
 
 
 if __name__ == "__main__":
