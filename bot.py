581b3517b868c6ef443e04cd084335e8d39d3df0
DavidKoban <49913976+DavidKoban@users.noreply.github.com>
2022-08-08 20:16:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,13 +11,8 @@ bot = telebot.TeleBot('5344051604:AAHl0HVkNG6UEX0h0DYR7nqnwPTSnKanzwI')
 
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+    bot.send_message(m.chat.id, 'Каждый день в 10:00 я буду присылать вам есть ли заказы у которых закончился срок поставки')
     User.objects.create(telegram_id=m.chat.id)
-
-
-@bot.message_handler(content_types=["text"])
-def handle_text(message):
-    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
 
 
 if __name__ == '__main__':
