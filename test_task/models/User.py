d198cd96e56e5541f724b0c3ff18ef95d186b57b
DavidKoban <49913976+DavidKoban@users.noreply.github.com>
2022-08-09 10:07:18 +0300

    Update User.py

--- test_task/models/User.py
+++ test_task/models/User.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 
+#Модель для хранения id чатов для отсыслки сообщений
 class User(models.Model):
     telegram_id = models.IntegerField(default=000000000, null=False)
