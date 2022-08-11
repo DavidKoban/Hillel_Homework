b73e8b92675a11bd7f4b88c512aa14ad25142468
DavidKoban <49913976+DavidKoban@users.noreply.github.com>
2022-08-11 21:04:14 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -41,7 +41,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['test_task.templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
